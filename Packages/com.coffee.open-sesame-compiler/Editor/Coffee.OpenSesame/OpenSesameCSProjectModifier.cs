10bb18f44bd47ed7bb0ec9e0205c53b7636b8815
mob-sakai <sakai@mobcast.jp>
2020-01-30 13:56:41 +0900

    refactor: change file name

---
+++ Packages/com.coffee.open-sesame-compiler/Editor/Coffee.OpenSesame/OpenSesameCSProjectModifier.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEditor.Compilation;
+
+namespace Coffee.OpenSesame
+{
+    internal class OpenSesameCSProjectModifier : AssetPostprocessor
+    {
+        static void Log(string format, params object[] args)
+        {
+            if (Core.LogEnabled)
+                UnityEngine.Debug.LogFormat("<color=#0063b1><b>[OpenSesameCSProjectModifier]</b></color> " + format, args);
+        }
+
+        static string OnGeneratedCSProject(string path, string content)
+        {
+            var assemblyName = Path.GetFileNameWithoutExtension(path);
+            var asmdefPath = CompilationPipeline.GetAssemblyDefinitionFilePathFromAssemblyName(assemblyName);
+            var setting = OpenSesameSetting.GetAtPath(asmdefPath);
+            if (string.IsNullOrEmpty(setting.ModifySymbols) && !setting.OpenSesame)
+                return content;
+
+            var defines = Regex.Match(content, "<DefineConstants>(.*)</DefineConstants>").Groups[1].Value.Split(';', ',');
+            defines = Core.ModifyDefines(defines, setting.OpenSesame, setting.ModifySymbols);
+            var defineText = string.Join(";", defines);
+
+            Log("Script defines in {0}.csproj are modified:\n{1}", assemblyName, defineText);
+            return Regex.Replace(content, "<DefineConstants>(.*)</DefineConstants>", string.Format("<DefineConstants>{0}</DefineConstants>", defineText));
+        }
+    }
+}
