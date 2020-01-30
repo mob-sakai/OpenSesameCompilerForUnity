f72f635d9ee7d188a121a9de8d9dfa80b53fafc5
mob-sakai <sakai@mobcast.jp>
2020-01-30 13:56:03 +0900

    feat: add portable mode

---
+++ Packages/com.coffee.open-sesame-compiler/Editor/Coffee.OpenSesame/OpenSesameInspectorGUI.cs
@@ -0,0 +1,124 @@
+using System.IO;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+namespace Coffee.OpenSesame
+{
+    [InitializeOnLoad]
+    internal static class OpenSesameInspectorGUI
+    {
+        static GUIContent s_OpenSesameText;
+        static GUIContent s_PortableModeText;
+        static GUIContent s_ModifySymbolsText;
+        static GUIContent s_SettingsText;
+        static GUIContent s_PublishText;
+        static GUIContent s_HelpText;
+        static bool s_OpenSettings = false;
+
+        static OpenSesameInspectorGUI()
+        {
+            s_OpenSesameText = new GUIContent("Open Sesame", "Use OpenSesameCompiler instead of default csc. In other words, allow access to internals and privates in other assemblies.");
+            s_ModifySymbolsText = new GUIContent("Modify Symbols", "When compiling this assembly, add or remove specific symbols separated with semicolons (;) or commas (,).\nSymbols starting with '!' will be removed.\n\ne.g. 'SYMBOL_TO_ADD;!SYMBOL_TO_REMOVE;...'");
+            s_PortableModeText = new GUIContent("Portable Mode", "Make this assembly available to other projects that do not have 'Open Sesame Compiler' package installed.");
+            s_SettingsText = new GUIContent("Settings", "Show other settings for this assembly.");
+            s_PublishText = new GUIContent("Publish", "Publish this assembly as dll to the parent directory.");
+            s_HelpText = new GUIContent("Help", "Open help page on browser.");
+
+            Editor.finishedDefaultHeaderGUI += OnPostHeaderGUI;
+            s_OpenSettings = EditorPrefs.GetBool("OpenSesameInspectorGUI_OpenSettings", false);
+        }
+
+        static void OnPostHeaderGUI(Editor editor)
+        {
+            var importer = editor.target as AssemblyDefinitionImporter;
+            if (!importer)
+                return;
+
+            bool settingChanged = false;
+            OpenSesameSetting setting = OpenSesameSetting.CreateFromJson(importer.userData);
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                // Enable OpenSesame.
+                using (var ccs = new EditorGUI.ChangeCheckScope())
+                {
+                    setting.OpenSesame = EditorGUILayout.ToggleLeft(s_OpenSesameText, setting.OpenSesame, GUILayout.MaxWidth(100));
+                    settingChanged |= ccs.changed;
+                }
+
+                GUILayout.FlexibleSpace();
+
+                // Open settings.
+                using (var ccs = new EditorGUI.ChangeCheckScope())
+                {
+                    s_OpenSettings = GUILayout.Toggle(s_OpenSettings, s_SettingsText, EditorStyles.miniButtonLeft);
+                    if (ccs.changed)
+                        EditorPrefs.SetBool("OpenSesameInspectorGUI_OpenSettings", s_OpenSettings);
+                }
+
+                // Publish assembly as a dll.
+                if (GUILayout.Button(s_PublishText, EditorStyles.miniButtonMid))
+                {
+                    OpenSesameSetting.AssemblyNameToPublish = GetAssemblyName(importer.assetPath);
+                    UnityEngine.Debug.Log(Core.kLogHeader + "Request to publish the assembly as dll: " + OpenSesameSetting.AssemblyNameToPublish);
+                    settingChanged = true;
+                }
+
+                // Open help.
+                if (GUILayout.Button(s_HelpText, EditorStyles.miniButtonRight))
+                {
+                    Application.OpenURL("https://github.com/mob-sakai/OpenSesameCompilerForUnity");
+                }
+            }
+
+                EditorGUI.indentLevel++;
+            if (s_OpenSettings)
+            {
+                // Modify symbols.
+                using (var ccs = new EditorGUI.ChangeCheckScope())
+                {
+                    setting.ModifySymbols = EditorGUILayout.DelayedTextField(s_ModifySymbolsText, setting.ModifySymbols);
+                    settingChanged |= ccs.changed;
+                }
+
+                // Portable mode.
+                using (new EditorGUI.DisabledScope(!setting.OpenSesame))
+                using (var ccs = new EditorGUI.ChangeCheckScope())
+                {
+                    var dllPath = Path.Combine(Path.GetDirectoryName(importer.assetPath), "OpenSesame.Portable.dll");
+                    EditorGUILayout.ToggleLeft(s_PortableModeText, File.Exists(dllPath));
+                    if (ccs.changed)
+                        EditorApplication.delayCall += () => SwitchPortableMode(dllPath);
+                }
+            }
+            EditorGUI.indentLevel--;
+
+            if (settingChanged)
+            {
+                importer.userData = JsonUtility.ToJson(setting);
+                importer.SaveAndReimport();
+            }
+        }
+
+        static void SwitchPortableMode(string dllPath)
+        {
+            var originPath = FileUtil.GetProjectRelativePath(typeof(OpenSesameInspectorGUI).Assembly.Location);
+            if (File.Exists(dllPath))
+                AssetDatabase.DeleteAsset(dllPath);
+            else
+            {
+                File.Copy(originPath, dllPath, true);
+                if (File.Exists(originPath + ".meta"))
+                    File.Copy(originPath + ".meta", dllPath + ".meta", true);
+                AssetDatabase.ImportAsset(dllPath);
+            }
+        }
+
+        static string GetAssemblyName(string asmdefPath = "")
+        {
+            var m = Regex.Match(File.ReadAllText(asmdefPath), "\"name\":\\s*\"([^\"]*)\"");
+            return m.Success ? m.Groups[1].Value : "";
+        }
+    }
+}
--- Packages/com.coffee.open-sesame-compiler/Editor/Coffee.OpenSesame/OpenSesameSetting.cs
+++ Packages/com.coffee.open-sesame-compiler/Editor/Coffee.OpenSesame/OpenSesameSetting.cs
@@ -1,168 +1,62 @@
 ﻿using System.IO;
 using UnityEditor;
-using UnityEditorInternal;
-using UnityEngine;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System;
 
 namespace Coffee.OpenSesame
 {
     [System.Serializable]
     internal class OpenSesameSetting
     {
-        const string k_KeyPublishOrigin = "OpenSesame_PublishOrigin";
-        const string k_KeyPublishAssemblyName = "OpenSesame_PublishAssemblyName";
-
-        public static string PublishOrigin
-        {
-            get { return EditorPrefs.GetString(k_KeyPublishOrigin, "") ?? ""; }
-            set { EditorPrefs.SetString(k_KeyPublishOrigin, value); }
-        }
-
-        public static string PublishAssemblyName
-        {
-            get { return EditorPrefs.GetString(k_KeyPublishAssemblyName, "") ?? ""; }
-            set { EditorPrefs.SetString(k_KeyPublishAssemblyName, value); }
-        }
-
-        public bool OpenSesame = false;
+        public static string AssemblyNameToPublish;
+        public bool OpenSesame;
         public string ModifySymbols = "";
 
-        [InitializeOnLoadMethod]
-        static void OnInitializeOnLoadMethod()
-        {
-            var origin = PublishOrigin;
-            var assemblyName = PublishAssemblyName;
-            PublishOrigin = null;
-            PublishAssemblyName = null;
-            if (!string.IsNullOrEmpty(origin) && !string.IsNullOrEmpty(assemblyName))
-            {
-                var src = "Library/ScriptAssemblies/" + assemblyName;
-                var dst = Path.Combine(Path.GetDirectoryName(origin.TrimEnd('/')), assemblyName);
-                UnityEngine.Debug.LogFormat("<b>[OpenSesame]</b> Publish assembly as dll: {0} -> {1}", src, dst);
-                FileUtil.UnityFileCopy(src, dst, true);
-            }
-        }
+        public bool SholdChangeCompilerProcess { get { return OpenSesame || !string.IsNullOrEmpty(ModifySymbols); } }
+        public bool SholdModifyDefines { get { return OpenSesame || !string.IsNullOrEmpty(ModifySymbols); } }
 
-        public static OpenSesameSetting GetAtPathOrDefault(string path)
+        public static OpenSesameSetting GetAtPath(string path)
         {
-            if (string.IsNullOrEmpty(path) || !File.Exists(Path.GetFullPath(path)))
-                return new OpenSesameSetting();
+            var setting = new OpenSesameSetting();
+            if (string.IsNullOrEmpty(path))
+                return setting;
 
-            var importer = AssetImporter.GetAtPath(path);
-            if (importer == null || string.IsNullOrEmpty(importer.userData))
-                return new OpenSesameSetting();
+            // If input path is directory, find asmdef file.
+            if (Directory.Exists(path))
+                path = Directory.GetFiles(path, "*.asmdef")
+                    .Select(x => x.Replace(Environment.CurrentDirectory + Path.DirectorySeparatorChar, ""))
+                    .FirstOrDefault();
+
+            // Not find asmdef file.
+            if (string.IsNullOrEmpty(path) || !File.Exists(path) || !File.Exists(path + ".meta"))
+                return setting;
 
             try
             {
-                return JsonUtility.FromJson<OpenSesameSetting>(importer.userData);
+                var json = AssetImporter.GetAtPath(path).userData;
+                GetOpenSesameSettingsFromJson(json, out setting.OpenSesame, out setting.ModifySymbols);
             }
-            catch
-            {
-                return new OpenSesameSetting();
-            }
+            catch { }
+            return setting;
         }
 
-        public static bool IsEnabled(string path)
+        public static OpenSesameSetting CreateFromJson(string json = "")
         {
-            if (string.IsNullOrEmpty(path) || !File.Exists(Path.GetFullPath(path)))
-                return false;
-
-            var importer = AssetImporter.GetAtPath(path);
-            if (importer == null || string.IsNullOrEmpty(importer.userData))
-                return false;
-
-            try
-            {
-                return JsonUtility.FromJson<OpenSesameSetting>(importer.userData).OpenSesame;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-    }
-
-    [InitializeOnLoad]
-    static class InspectorGUI
-    {
-        const string k_ShowSymbolsKey = "OpenSesame_ShowSymbols";
-
-        static GUIContent s_OpenSesameText;
-        static GUIContent s_ModifySymbolsText;
-        static GUIContent s_PublishText;
-        static GUIContent s_ShowSymbolsText;
-        static GUIContent s_HelpText;
-        static bool s_ShowSymbols;
-
-        static InspectorGUI()
-        {
-            s_OpenSesameText = new GUIContent("Open Sesame", "Use OpenSesameCompiler instead of default csc. In other words, allow access to internals/privates to other assemblies.");
-            s_ModifySymbolsText = new GUIContent("Modify Symbols", "When compiling this assembly, add/remove semicolon separated symbols. Symbols starting with '!' will be removed.");
-            s_PublishText = new GUIContent("Publish", "Publish this assembly as dll to the parent directory.");
-            s_ShowSymbolsText = new GUIContent("Symbols", "Show/hide the scripting define symbols to modify for this assembly.");
-            s_HelpText = new GUIContent("Help", "Open help page on browser.");
-            Editor.finishedDefaultHeaderGUI += OnPostHeaderGUI;
-            s_ShowSymbols = EditorPrefs.GetBool(k_ShowSymbolsKey, false);
+            var setting = new OpenSesameSetting();
+            GetOpenSesameSettingsFromJson(json, out setting.OpenSesame, out setting.ModifySymbols);
+            return setting;
         }
 
-        static void OnPostHeaderGUI(Editor editor)
+        static void GetOpenSesameSettingsFromJson(string json, out bool openSesame, out string modifySymbols)
         {
-            var importer = editor.target as AssemblyDefinitionImporter;
-            if (!importer)
+            openSesame = false;
+            modifySymbols = "";
+            if (string.IsNullOrEmpty(json))
                 return;
 
-            OpenSesameSetting setting = null;
-            var userdata = importer.userData;
-            try
-            {
-                setting = string.IsNullOrEmpty(userdata)
-                        ? new OpenSesameSetting()
-                        : JsonUtility.FromJson<OpenSesameSetting>(userdata);
-            }
-            catch
-            {
-                setting = new OpenSesameSetting();
-            }
-
-            using (new EditorGUILayout.HorizontalScope())
-            {
-                EditorGUI.BeginChangeCheck();
-                setting.OpenSesame = EditorGUILayout.ToggleLeft(s_OpenSesameText, setting.OpenSesame, GUILayout.MaxWidth(100));
-                if (EditorGUI.EndChangeCheck())
-                {
-                    importer.userData = JsonUtility.ToJson(setting);
-                    importer.SaveAndReimport();
-                }
-
-                GUILayout.FlexibleSpace();
-
-                if (GUILayout.Toggle(s_ShowSymbols, s_ShowSymbolsText, EditorStyles.miniButtonLeft) != s_ShowSymbols)
-                {
-                    s_ShowSymbols = !s_ShowSymbols;
-                    EditorPrefs.SetBool(k_ShowSymbolsKey, s_ShowSymbols);
-                }
-
-                if (GUILayout.Button(s_PublishText, EditorStyles.miniButtonMid))
-                {
-                    OpenSesameSetting.PublishOrigin = Path.GetDirectoryName(importer.assetPath).Replace('\\', '/') + "/";
-                    importer.SaveAndReimport();
-                }
-
-                if (GUILayout.Button(s_HelpText, EditorStyles.miniButtonRight))
-                {
-                    Application.OpenURL("https://github.com/mob-sakai/OpenSesameCompilerForUnity");
-                }
-            }
-
-            if (!s_ShowSymbols)
-                return;
-
-            EditorGUI.BeginChangeCheck();
-            setting.ModifySymbols = EditorGUILayout.DelayedTextField(s_ModifySymbolsText, setting.ModifySymbols);
-            if (EditorGUI.EndChangeCheck())
-            {
-                importer.userData = JsonUtility.ToJson(setting);
-                importer.SaveAndReimport();
-            }
+            openSesame = Regex.Match(json, "\"OpenSesame\":\\s*(true|false)").Groups[1].Value == "true";
+            modifySymbols = Regex.Match(json, "\"ModifySymbols\":\\s*\"([^\"]*)\"").Groups[1].Value;
         }
     }
 }
